561ba47507c1bead9d841d8df4d20f0a8abb6c1e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-12 12:56:27 +0530

    XAMARIN-38038 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+        SfListView ListView;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("ToDoListView");
+            ListView.ItemDragging += ListView_ItemDragging;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            ListView.ItemDragging -= ListView_ItemDragging;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Call back
+        private void ListView_ItemDragging(object sender, ItemDraggingEventArgs e)
+        {
+            if (e.Action == DragAction.Drop)
+            {
+                if (e.NewIndex < e.OldIndex)
+                {
+                    Device.BeginInvokeOnMainThread(() => ListView.RefreshListViewItem(-1, -1, true));
+                };
+            }
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/IndexConverter.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.ListView.XForms;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    #region IndexConverter
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+
+            var item = value as ToDoItem;
+            var listView = parameter as SfListView;
+            return listView.DataSource.DisplayItems.IndexOf(item);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
